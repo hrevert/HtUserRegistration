53458fb0a2e16fc8f70fe90f8998354220d3f90a
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2014-09-16 18:04:47 +0200

    Check if config entry exist (prevent exception)

--- src/Factory/ModuleOptionsFactory.php
+++ src/Factory/ModuleOptionsFactory.php
@@ -1,14 +1,17 @@
 <?php
+
 namespace HtUserRegistration\Factory;
 
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\ServiceManager\FactoryInterface;
 use HtUserRegistration\Options\ModuleOptions;
 
-class ModuleOptionsFactory implements FactoryInterface
-{
-    public function createService(ServiceLocatorInterface $serviceLocator)
-    {
-        return new ModuleOptions($serviceLocator->get('Config')['ht_user_registration']);
+class ModuleOptionsFactory implements FactoryInterface {
+
+    public function createService(ServiceLocatorInterface $serviceLocator) {
+        $config = $serviceLocator->get('Config');
+
+        return new ModuleOptions(isset($config['ht_user_registration']) ? $config['ht_user_registration'] : array());
     }
+
 }
